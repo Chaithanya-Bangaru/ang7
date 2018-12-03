8d16c93f89e20d4a75cf208a720f04ca7f51f673
blueskies <blueskiesgreenwaters@outlook.com>
2018-12-03 16:40:51 -0600

    marc wan ang7 course concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Recipe } from './model/recipe';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'rcb-app-root',
+  //template: `  <h1>
+  //This is the Recipe app
+  //</h1>
+  //`,
+  templateUrl : './app.component.html',
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
-  title = 'rapp';
+  title = 'Recipe App';
+  recipe: Recipe;
+  constructor(){
+    this.recipe = new Recipe('Banana Bread', 'My Favorite Banana bread recipe',null,null,null);
+  }
 }
---
+++ src/app/model/recipe.ts
@@ -0,0 +1,25 @@
+export interface Instruction {
+    instruction: string;
+    photo: string;
+}
+export interface Ingredient {
+    measure: string;
+    photo: string;
+}
+export class Recipe {
+    public title: string;
+    public description: string;
+    public ingredients: Ingredient[];
+    public instructions: Instruction[];
+    public cover_photo: string;
+
+    public constructor(t: string, d: string, ing: Ingredient[], ins: Instruction[], cp: string){
+        this.title = t;
+        this.description = d;
+        this.ingredients = ing;
+        this.instructions = ins;
+        this.cover_photo = cp;
+    }
+}
+
+
