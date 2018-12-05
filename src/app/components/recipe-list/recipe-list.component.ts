105573ae7678f40c075ba23a4bb94571609280e8
blueskies <blueskiesgreenwaters@outlook.com>
2018-12-05 00:23:27 -0600

    component event communication thru EventEmitter

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -5,20 +5,19 @@ import { Recipe } from '../../model/recipe';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent  {
+export class RecipeListComponent {
 
   recipes: Recipe[];
-  constructor(){
+  constructor() {
     this.recipes = [new Recipe('Banana Bread', 'My Favorite Banana bread recipe!I had'
-    +' this once in starbucks in a Minneapolis trip and from that moment'
-    +' it has become my favorite.',null,null,null,30,4),
-    new Recipe('Japanese Sushi', 'Had this in a Downtown restaurant '+
-    'in Omaha and it is good.',null,null,null,45,5),
-    new Recipe('Mutton Haleem', 'Had this in a SACFO fellowship dinner and it tasted really good.'+
-    ' Heard its made by Sahyad ',null,null,null,90,10)]
+      + ' this once in starbucks in a Minneapolis trip and from that moment'
+      + ' it has become my favorite.', null, null, null, 30, 4),
+    new Recipe('Japanese Sushi', 'Had this in a Downtown restaurant ' +
+      'in Omaha and it is good.', null, null, null, 45, 5),
+    new Recipe('Mutton Haleem', 'Had this in a SACFO fellowship dinner and it tasted really good.' +
+      ' Heard its made by Sahyad ', null, null, null, 90, 10)]
   }
-
-  public talkToOtherComponent(recipe){
-    console.log(JSON.stringify(recipe,null,2));
+  public talkToOtherComponent(recipe) {
+    console.log(JSON.stringify(recipe, null, 2));
   }
 }
