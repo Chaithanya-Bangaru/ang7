47beccf349f7ddede48e094e81728aecfd81a44e
blueskies <blueskiesgreenwaters@outlook.com>
2018-12-04 19:08:44 -0600

    sync up local changes

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -11,10 +11,10 @@ export class RecipeListComponent  {
   constructor(){
     this.recipes = [new Recipe('Banana Bread', 'My Favorite Banana bread recipe!I had'
     +' this once in starbucks in a Minneapolis trip and from that moment'
-    +' it has become my favorite.',null,null,null),
+    +' it has become my favorite.',null,null,null,30,4),
     new Recipe('Japanese Sushi', 'Had this in a Downtown restaurant '+
-    'in Omaha and it is good.',null,null,null),
+    'in Omaha and it is good.',null,null,null,45,5),
     new Recipe('Mutton Haleem', 'Had this in a SACFO fellowship dinner and it tasted really good.'+
-    ' Heard its made by Sahyad ',null,null,null)]
+    ' Heard its made by Sahyad ',null,null,null,90,10)]
   }
 }
--- src/app/model/recipe.ts
+++ src/app/model/recipe.ts
@@ -1,4 +1,4 @@
-export interface Instruction {
+export interface Instruction { // contract of what data type of field just like in java
     instruction: string;
     photo: string;
 }
@@ -6,19 +6,28 @@ export interface Ingredient {
     measure: string;
     photo: string;
 }
+
 export class Recipe {
     public title: string;
     public description: string;
     public ingredients: Ingredient[];
     public instructions: Instruction[];
     public cover_photo: string;
+    // not visible even to set the field in the value object
+    private _secretIngredient: string; 
+    public prepTime: number;
+    public feedsThisMany:  number;
 
-    public constructor(t: string, d: string, ing: Ingredient[], ins: Instruction[], coverpic: string){
+    //Same as Java constructor
+    public constructor(t: string, d: string, ing: Ingredient[]
+        , ins: Instruction[], coverpic: string,prepTime: number, feedsThisMany:  number){
         this.title = t;
         this.description = d;
         this.ingredients = ing;
         this.instructions = ins;
         this.cover_photo = coverpic;
+        this.prepTime = prepTime;
+        this.feedsThisMany = feedsThisMany;
     }
 }
 
